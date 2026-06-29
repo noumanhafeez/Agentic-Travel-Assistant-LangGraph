0cb22cebb2aff975a2a5e931f478e9f28af592a3
noumanhafeez <noumanhafeez095@gmail.com>
2026-06-29 17:34:54 +0500

    feat: define state for graph

---
+++ schemas/state.py
@@ -0,0 +1,16 @@
+from typing import TypedDict, Annotated
+from langchain_core.messages import AnyMessage
+import operator
+
+
+class TravelState(TypedDict):
+    messages: Annotated[list[AnyMessage], operator.add]
+
+    user_query: str
+
+    flight_results: str
+    hotel_results: str
+
+    itinerary: str
+
+    llm_calls: int
