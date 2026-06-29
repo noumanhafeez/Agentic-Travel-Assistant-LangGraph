4ec85644226aa9e53e9d482cb1c4c7679feb95e2
noumanhafeez <noumanhafeez095@gmail.com>
2026-06-29 17:30:38 +0500

    feat: add final agent for generate final output

---
+++ agents/final.py
@@ -0,0 +1,45 @@
+from langchain_core.messages import (
+    HumanMessage
+)
+
+
+
+def final_agent(state):
+
+
+    final_prompt = f"""
+        Generate final travel response.
+
+        Flights:
+        {state['flight_results']}
+
+        Hotels:
+        {state['hotel_results']}
+
+        Itinerary:
+        {state['itinerary']}
+        """
+
+    response = llm.invoke(
+
+        [
+
+            HumanMessage(
+                content=final_prompt
+            )
+
+        ]
+
+    )
+
+    return {
+
+        "messages":
+        [response],
+
+        "llm_calls":
+        state.get(
+            "llm_calls",
+            0
+        ) + 1
+    }
